73880eee90b9cf96965ad8ed1cd1cb31f5214ecb
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-06-29 15:51:30 -0300

    Calling spider from inside main file

--- src/main.py
+++ src/main.py
@@ -1,20 +1,28 @@
 from datetime import date
 from dotenv import load_dotenv
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
 from mongo.experience_repository import ExperienceMongoRepository
+
+from scraper.scraper import settings as scraper_settings
+from scraper.scraper.spiders.experiencespider import (
+    ExperienceSpider,
+)
+
+
+def call_spider():
+    crawler_settings = Settings()
+    crawler_settings.setmodule(scraper_settings)
+
+    process = CrawlerProcess(settings=crawler_settings)
+
+    process.crawl(ExperienceSpider)
+    process.start()
 
 
 def main():
-    experience_repository = ExperienceMongoRepository()
-
-    experience = experience_repository.create(
-        date=date.today().strftime(format="%d/%m/%Y"),
-        person_name="João",
-        church="JC Mogi Mirim",
-        content="This is my Experience",
-        audio_url="some url",
-        url="some url",
-    )
-    print(experience)
+    call_spider()
 
 
 if __name__ == "__main__":
