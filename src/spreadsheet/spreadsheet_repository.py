365edd5b8d39ab3cf10d50715ab10343771f2d0d
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-07-04 19:43:45 -0300

    Adding uuid to spreadsheet

--- src/spreadsheet/spreadsheet_repository.py
+++ src/spreadsheet/spreadsheet_repository.py
@@ -1,5 +1,6 @@
 import gspread
 import os
+from uuid import uuid4
 from oauth2client.service_account import ServiceAccountCredentials
 
 from models.experience import Experience
@@ -31,6 +32,7 @@ class SpreadSheetRepository:
     ) -> Experience:
         sheet = self.client.open(SPREADSHEET_FILENAME).sheet1
         experience_obj = [
+            str(uuid4()),
             date,
             person_name,
             church,
@@ -40,3 +42,18 @@ class SpreadSheetRepository:
         ]
 
         sheet.append_row(experience_obj)
+
+    def getByDate(self, date) -> Experience:
+        sheet = self.client.open(SPREADSHEET_FILENAME).sheet1
+
+        all_values = sheet.get_all_values()
+        latest_experience = all_values[-1]
+        return Experience(
+            _id=latest_experience[0],
+            date=latest_experience[1],
+            person_name=latest_experience[2],
+            church=latest_experience[3],
+            content=latest_experience[4],
+            audio_url=latest_experience[5],
+            url=latest_experience[6],
+        )
