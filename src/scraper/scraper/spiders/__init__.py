836c4d4bedd45e473ea51c60904ea91d0c06fcdb
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-07-09 15:50:40 -0300

    Exporting spiders on __init__ file

--- src/scraper/scraper/spiders/__init__.py
+++ src/scraper/scraper/spiders/__init__.py
@@ -2,3 +2,6 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+
+from .experiencespider import ExperienceSpider
+from .sacredwordspider import SacredWordSpider
