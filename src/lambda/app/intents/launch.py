02920a7a16ae2301fac4fbb11cb400ac44973947
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-07-30 11:58:52 -0300

    Removing 'Bom dia' phrase

--- src/lambda/app/intents/launch.py
+++ src/lambda/app/intents/launch.py
@@ -16,7 +16,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speak_output = "Olá bom dia! Qual ensinamento gostaria que eu lesse?"
+        speak_output = "Olá! Qual ensinamento gostaria que eu lesse?"
 
         return (
             handler_input.response_builder.speak(speak_output)
