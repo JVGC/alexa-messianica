fb87437b355620d64805eb8192041a9889a7e201
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-09-18 20:16:06 -0300

    Handling error when date is not found

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -4,11 +4,40 @@ from datetime import date
 from scraper.spreadsheet import create_sacred_word
 
 
+MONTHS = {
+    "janeiro": "01",
+    "fevereiro": "02",
+    "março": "03",
+    "abril": "04",
+    "maio": "05",
+    "junho": "06",
+    "julho": "07",
+    "agosto": "08",
+    "setembro": "09",
+    "outubro": "10",
+    "novembro": "11",
+    "dezembro": "12",
+}
+
+
 class InsertPipeline:
     def __init__(self) -> None:
         pass
 
     def process_item(self, item, _):
+        item["date"] = item["date"].split(" ")
+
+        item["date"][1] = MONTHS[item["date"][1].lower()]
+
+        if (
+            item["date"][0].lstrip("0") != str(date.today().day)
+            and item["date"][1].lstrip("0") != str(date.today().month)
+            and item["date"][2] != str(date.today().year)
+        ):
+            print("Today's sacred word is not available yet")
+            return item
+
+        print("Scrapping today's sacred word")
         create_sacred_word(
             date=date.today().strftime(format="%d/%m/%Y"),
             title=item["title"],
--- scraper/scraper/spiders/sacredwordspider.py
+++ scraper/scraper/spiders/sacredwordspider.py
@@ -15,7 +15,8 @@ class SacredWordSpider(Spider):
             .css("h2.calendar-card__content-title::text")
             .get()
             .strip(),
-            "url": f"{self.start_urls[0]}?d={date.today().strftime(format='%d/%m/%Y')}",
+            "date": response.css("h1::text").get().strip(),
+            "url": self.start_urls[0],
             "audio_url": f"https://www.messianica.org.br{response.css('audio#player').css('source').xpath('@src').get()}",
             "content": "".join(response.css("section#wa-conteudo::text").getall())
             .strip()
