4bf431180e078309142c5c09fcbc5c4cc03cb581
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2024-06-03 08:06:40 -0300

    Remove Scripts

--- scripts/scrape.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PATH="/home/ubuntu/.local/bin:$PATH"
-cd scraper
-poetry run scrapy crawl sacred_word
--- scripts/setup-cronjob.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-crontab -r
-
-( ( crontab -l ; echo "0 * * * * cd /home/ubuntu/alexa-messianica && make scrape" ) | crontab - ) >& /dev/null
-
-
-crontab -l
