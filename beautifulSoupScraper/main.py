96fdb310587e43248c69bdf4d0cd233e15761326
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2024-06-06 09:11:12 -0300

    Improve main script

--- beautifulSoupScraper/main.py
+++ beautifulSoupScraper/main.py
@@ -1,6 +1,12 @@
+from time import sleep
 from beautifulSoupScraper.sacred_word_scraper import SacredWordScraper
 
 
 spider = SacredWordScraper()
 data = spider.scrape_data()
 spider.process_item(data)
+
+while data:
+    data = spider.scrape_data()
+    spider.process_item(data)
+    sleep(2)
